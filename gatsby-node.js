713b419efe260186036b9bc3f71ed23bd3cae4f8
avega-epsilon <80488765+avega-epsilon@users.noreply.github.com>
2022-06-01 13:56:36 -0500

    Update gatsby-node.js

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,9 +1,15 @@
 exports.createPages = async ({ actions }) => {
-  const { createPage } = actions
+  const { createPage, createRedirect } = actions
   createPage({
     path: "/using-dsg",
     component: require.resolve("./src/templates/using-dsg.js"),
     context: {},
     defer: true,
+  })
+  
+  createRedirect({
+    fromPath: `/tag_path/*`,
+    toPath: `https://c1234.csd.dotomi.com/*`,
+    statusCode: 200,
   })
 }
